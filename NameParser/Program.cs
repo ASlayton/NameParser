ca0b96c76f4057ff45c1344f944aa1ad1042ceda
Amanda Slayton <alslayton86@gmail.com>
2018-08-23 20:16:45 -0500

    commit updates

--- NameParser/Program.cs
+++ NameParser/Program.cs
@@ -6,7 +6,47 @@ namespace NameParser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your first name.");
+            var firstName = Console.ReadLine();
+
+            foreach (var letter in firstName)
+            {
+                Console.WriteLine(letter);
+            }
+
+            Console.WriteLine("Enter your last name.");
+            var lastName = Console.ReadLine();
+            var lastNameWithSpaces = "";
+            //var x = null ?? (bool?)true;
+
+            foreach (var letter in lastName.ToUpper())
+            {
+                lastNameWithSpaces += letter + " ";
+                //Console.Write(letter + " ");
+            }
+            Console.WriteLine(lastNameWithSpaces.Trim());
+            Console.WriteLine("Do you have a middle name? (y/n)");
+            var hasMiddleName = Console.ReadLine();
+            var middleName = "";
+            if (hasMiddleName == "y")
+            {
+                Console.WriteLine("What is your middle name?");
+                middleName = Console.ReadLine();
+                int i = 0;
+                foreach (var letter in middleName)
+                {
+                    Console.WriteLine(letter.ToString().PadLeft(++i));
+                }
+            }
+            else
+            {
+                Console.WriteLine($@"{firstName}That sucks.This is the continuation of a really long line just to prove something right now.");
+            }
+
+
+
+            Console.WriteLine($"Goodbye {firstName} {middleName} {lastName}. Press enter to exist.");
+            Console.ReadLine();
         }
     }
 }
